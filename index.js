63faddc32c3863dda01c12070b8c318a8e5d3279
jyanev01 <jordanyanev@gmail.com>
2021-11-06 14:32:00 -0400

    builTeam()

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const fs = require('fs');
 const inquirer = require("inquirer");
+const render = require('./src/page-template');
 
 const employeeArr = [];
 
@@ -77,9 +78,6 @@ const promptRole = () => {
     ])
 }
 
-askManager();
-// mainMenu();
-
 
 // Enineer question porompts
 const askEngineer = () => {
@@ -151,39 +149,21 @@ const askIntern = () => {
 
 }
 
+askManager();
 
-// askEngineer();
-// askIntern();
+// function buildTeam() {
+//     // Create the output directory if the output path doesn't exist
+//     if (!fs.existsSync(OUTPUT_DIR)) {
+//       fs.mkdirSync(OUTPUT_DIR)
+//     }
+//     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+// }
 
 // const writeFile = employeeArr => {
 //     fs.writeFile('index.html', employeeArr, err => {
 //         if (err) {
 //             console.log(err);
-//             return 
-//             `
-//             <!DOCTYPE html>
-//             <html lang="en">
-//                 <head>
-//                     <meta charset="UTF-8">
-//                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//                     <title>Team Profiles</title>
-//                 </head>
-//                 <body>
-                
-                    
-                    
-//                 </body>
-//                 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-//                 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-//                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-//                 </html>
-            
-            
-            
-            
-//             ` 
-//             ;
+//             return;
 //         } else {
 //             console.log( "Team profile has been successfully created!")
 //         }
